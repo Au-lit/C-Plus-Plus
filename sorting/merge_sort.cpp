4522bf5e5ee042b24c07923b204fa0076a1aa560
Au Lit <68131161+Au-lit@users.noreply.github.com>
2021-06-03 12:29:13 -0400

    removed raw delete/new

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -14,7 +14,7 @@
  *
  */
 #include <iostream>
-
+#include <vector>
 /**
  *
  * The merge() function is used for merging two halves.
@@ -31,19 +31,14 @@
  * @param l - end index or right index of second half array
  */
 void merge(int *arr, int l, int m, int r) {
-    int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    int *L = new int[n1], *R = new int[n2];
+    std::vector<int> L { n1 }, R { n2 };
 
-    for (i = 0; i < n1; i++) L[i] = arr[l + i];
-    for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 || j < n2) {
+    for (int i = 0; i < n1; ++i) L[i] = arr[l + i];
+    for (int j = 0; j < n2; ++j) R[j] = arr[m + 1 + j];
+    for (int i = 0, j = 0, k = l; i < n1 || j < n2; ++k) {
         if (j >= n2 || (i < n1 && L[i] <= R[j])) {
             arr[k] = L[i];
             i++;
@@ -51,11 +46,7 @@ void merge(int *arr, int l, int m, int r) {
             arr[k] = R[j];
             j++;
         }
-        k++;
     }
-
-    delete[] L;
-    delete[] R;
 }
 
 /**
